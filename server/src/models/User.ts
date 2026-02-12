92c6be9522f0aeb02b028e7eb59715f8c50d3363
AmberPenguin24 <140901179+AmberPenguin24@users.noreply.github.com>
2026-02-12 14:42:20 -0600

    Updated the Mongo table for User
    Includes/Embeds the Stats into the User
    
    We only need to access the stats from the profile page
    
    Haven't tested yet -Need to update VS

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,15 +1,45 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
+/**
+ * SOURCE OF TRUTH: MongoDB
+ * This model handles persistent user data. 
+ * Use this for: Login, Stats, and Profile settings.
+ */
+
+// PERSISTENT: Matches 'Player' and 'Stats' from the ER Diagram.
+// Lives in MongoDB. Used for long-term tracking and login.
+
 export interface IUser extends mongoose.Document {
   username: string;
   password: string;
+  profilePhotoURL?: string;
+  stats: {
+    gamesPlayed: number;
+    gamesWon: number;
+    gamesLost: number;
+    totalBidsCast: number;
+    totalBidsWon: number;
+    totalBidSuccess: number;
+  }
+
   comparePassword: (candidate: string) => Promise<boolean>;
 }
 
 const userSchema = new mongoose.Schema<IUser>({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  profilePhotoURL: { type: String, default: "" },
+  //Nested Stats object to keep all stats in one place. This allows for easy expansion in the future if we want to add more stats.
+  stats: {
+    gamesPlayed: { type: Number, default: 0 },
+    gamesWon: { type: Number, default: 0 },
+    gamesLost: { type: Number, default: 0 },
+    winLossRatio: { type: Number, default: 0 },
+    totalBidsCast: { type: Number, default: 0 },
+    totalBidsWon: { type: Number, default: 0 },
+    totalBidSuccess: { type: Number, default: 0 },
+  }
 });
 
 // Hash password before saving
