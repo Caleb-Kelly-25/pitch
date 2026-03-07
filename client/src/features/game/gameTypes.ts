77fff3e0edce5b8bb4c3fa6fa2b0465a028eaf43
Caleb-Kelly-25 <ckelly25@huskers.unl.edu>
2026-03-06 21:05:11 -0600

    Update gameTypes.ts

--- client/src/features/game/gameTypes.ts
+++ client/src/features/game/gameTypes.ts
@@ -1,6 +1,50 @@
 export interface GameState {
-    id: string | null
-    players: string[]
-    hand: string[]
-    currentPlayer: string | null
+    gameId: string;
+    phase: "bidding" | "playing" | "scoring" | "waiting";
+    players: Player[];
+    hand: CardModel[];
+
+    trick: {
+        leadPlayerId: string;
+        playedCards: [
+            {playerId:string, card: CardModel}
+        ]
+    }
+
+    bidding: {
+        currentBidderId: string;
+        highestBidderId: string;
+        bids: {
+            playerId: number | "pass";
+        }
+    }
+
+    trickNumber: number;
+    leadSuit: "hearts" | "diamonds" | "clubs" | "spades" | null;
+    scores: number[]
+}
+
+export interface Player {
+    id: string;
+    username: string;
+    seat: 1 | 2 | 3 | 4;
+    team: 0 | 1;
+    isDealer: boolean;
+    isConnected: boolean;
+    cardCount: number;
+}
+
+
+type Suit = "hearts" | "diamonds" | "clubs" | "spades";
+type Value = "A" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K";
+export interface CardModel {
+    suit: Suit;
+    value: Value;
+}
+
+export interface Trick {
+    leadPlayerId: string;
+    playedCards: [
+        {playerId: string, card: CardModel}
+    ]
 }
