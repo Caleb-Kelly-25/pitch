1c6b21c1a0ff59ab5eb90ec59e7ae12707301c5a
Caleb-Kelly-25 <ckelly25@huskers.unl.edu>
2026-04-19 15:37:13 -0500

    Change variable name

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -12,7 +12,7 @@ data "aws_caller_identity" "current" {}
 provider "aws" {
     region = "us-east-2"
     assume_role {
-        role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-deploy-role-2"
+        role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GitHub-deploy-role"
         session_name = "GitHub-Infra-Deploy"
     }
 }
