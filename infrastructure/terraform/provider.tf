379830b9df7f82f2cb39598b49d1072d89f617d7
Caleb-Kelly-25 <ckelly25@huskers.unl.edu>
2026-04-19 15:49:14 -0500

    Back to east-1

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -10,7 +10,7 @@ terraform{
 data "aws_caller_identity" "current" {}
 
 provider "aws" {
-    region = "us-east-2"
+    region = "us-east-1"
     assume_role {
         role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/GitHub-deploy-role"
         session_name = "GitHub-Infra-Deploy"
