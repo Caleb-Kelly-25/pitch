c2d2c094a5d3e64c90a0fda169e064df2f11b154
Caleb-Kelly-25 <ckelly25@huskers.unl.edu>
2026-04-19 22:00:38 -0500

    Restored provider

--- infrastructure/terraform/route53.tf
+++ infrastructure/terraform/route53.tf
@@ -42,7 +42,6 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "frontend" {
-  provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.frontend.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
