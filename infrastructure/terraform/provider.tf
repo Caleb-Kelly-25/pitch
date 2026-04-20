a9c48e4590d751d64ea9dc9c203b637ed48acb9c
Caleb-Kelly-25 <ckelly25@huskers.unl.edu>
2026-04-19 19:32:03 -0500

    Removed unnecessary assume role

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -11,8 +11,9 @@ data "aws_caller_identity" "current" {}
 
 provider "aws" {
     region = "us-east-1"
-    assume_role {
-        role_arn = "arn:aws:iam::${var.account_id}:role/GitHub-deploy-role"
-        session_name = "GitHub-Infra-Deploy"
-    }
+    # This should be unnecessary
+    # assume_role {
+    #     role_arn = "arn:aws:iam::${var.account_id}:role/GitHub-deploy-role"
+    #     session_name = "GitHub-Infra-Deploy"
+    # }
 }
